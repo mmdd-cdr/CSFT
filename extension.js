e4ecdb3f61484954f87082f461941f1c491466bd
czx <742046286@qq.com>
2022-12-07 16:40:13 +0800

    第一版本

---
+++ extension.js
@@ -0,0 +1,168 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require("vscode");
+const fs = require("fs");
+const lodash = require("lodash");
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+	let disposable = vscode.commands.registerCommand("csft.cft", async (uri) => {
+		const templates =  vscode.workspace.getConfiguration().get("csft.templatesPath");
+		const fileName = await promptForParam("文件名称");
+		if(!fileName){
+			return;
+		}
+		const createPath = await getTargetDirectory(uri, fileName);
+		let folderPath = vscode.workspace.workspaceFolders;
+		let fsPath;
+		if (uri) {
+			fsPath = uri.fsPath;
+		} else {
+			fsPath = folderPath[0].uri.fsPath;
+		}
+		let fsPathUri = vscode.Uri.file(`${fsPath}/${templates}`);
+		let fileNameList = await vscode.workspace.fs.readDirectory(fsPathUri);
+		if (fileNameList.length === 0) {
+			vscode.window.showErrorMessage("找不到模版文件");
+			return;
+		}
+		const templatesName = await promptForUseEquatable(fileNameList);
+		fs.readFile(`${fsPath}/${templates}/${templatesName}`, "utf-8", async (err, dataStr) => {
+			const getTemplateConfig = new Function(dataStr);
+			try {
+				const {
+					templateInterface,
+					template
+				} = getTemplateConfig();
+				const paramList = {}
+				await Promise.all(
+					templateInterface.map(async (item) => {
+						paramList[item.key] = await promptForParam(item.label);
+					})
+				)
+				await createFileFormTemplate({
+					createPath,
+					fileName,
+					template,
+					param: paramList
+				});
+				const openWhenFinished = vscode.workspace.getConfiguration().get("csft.openWhenFinished");
+				if(openWhenFinished){
+					const openUri = vscode.Uri.file(`${createPath}/${fileName}`);
+					vscode.workspace.openTextDocument(openUri).then((doc) => {
+						vscode.window.showTextDocument(doc)
+					},(err)=>{
+						vscode.window.showErrorMessage(err);
+					});
+				}
+			} catch (err) {
+				vscode.window.showErrorMessage(err);
+			}
+		});
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// 选择使用的模版
+async function promptForUseEquatable(fileNameList) {
+	const useEquatablePromptValues = fileNameList.map((item) => {
+		return item[0];
+	});
+
+	const useEquatablePromptOptions = {
+		placeHolder: "请选择创建页面使用的模版?",
+		canPickMany: false,
+	};
+
+	const answer = await vscode.window.showQuickPick(
+		useEquatablePromptValues,
+		useEquatablePromptOptions
+	);
+
+	return answer;
+}
+
+// 监听输入参数
+async function promptForParam(label) {
+	const contextNamePromptOptions = {
+		prompt: `输入${label}`,
+		placeHolder: `请输入${label}`,
+	};
+	return vscode.window.showInputBox(contextNamePromptOptions);
+}
+
+// 判断当前是否是有效目录
+async function getTargetDirectory(uri, fileName) {
+	let targetDirectory;
+	if (lodash.isNil(lodash.get(uri, "fsPath")) || !fs.lstatSync(uri.fsPath).isDirectory()) {
+		targetDirectory = await promptForTargetDirectory(fileName);
+		if (lodash.isNil(targetDirectory)) {
+			throw Error("请选择有效的目录");
+		}
+	} else {
+		targetDirectory = uri.fsPath;
+	}
+
+	return targetDirectory;
+}
+
+// 选择目录
+async function promptForTargetDirectory(fileName) {
+	const options = {
+		canSelectMany: false,
+		openLabel: `将${fileName}创建到这个目录`,
+		canSelectFolders: true,
+	};
+
+	return vscode.window.showOpenDialog(options).then((uri) => {
+		if (lodash.isNil(uri) || lodash.isEmpty(uri)) {
+			return undefined;
+		}
+		return uri[0].fsPath;
+	});
+}
+
+// 将模版写入文件
+async function createFileFormTemplate({
+	createPath,
+	fileName,
+	template,
+	param
+}) {
+	const setFileExtendedName =  vscode.workspace.getConfiguration().get("csft.setFileExtendedName");
+	let targetPath = `${createPath}/${fileName}`;
+	if(setFileExtendedName){
+		targetPath = `${targetPath}.${setFileExtendedName}`
+	}
+	if (fs.existsSync(targetPath)) {
+		throw Error(`已经存在文件${fileName}`);
+	}
+	return new Promise(async (resolve, reject) => {
+		fs.writeFile(
+			targetPath,
+			template(param),
+			"utf8",
+			(error) => {
+				if (error) {
+					reject(error);
+					return;
+				}
+				resolve('');
+			}
+		);
+	});
+};
+
+module.exports = {
+	activate,
+	promptForUseEquatable,
+	promptForParam,
+	getTargetDirectory,
+	promptForTargetDirectory,
+
+};
